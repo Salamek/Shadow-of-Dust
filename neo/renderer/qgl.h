1948a18119a711f8ef8c2949fa2475c2cf245774
Adam Schubert <feartohell@seznam.cz>
2012-01-04 20:38:23 +0100

    GL Fix

--- neo/renderer/qgl.h
+++ neo/renderer/qgl.h
@@ -1,25 +1,25 @@
 /*
 ===========================================================================
 
-Shadow of Dust GPL Source Code
+Doom 3 GPL Source Code
 Copyright (C) 1999-2011 id Software LLC, a ZeniMax Media company.
 
-This file is part of the Shadow of Dust GPL Source Code ("Shadow of Dust Source Code").
+This file is part of the Doom 3 GPL Source Code ("Doom 3 Source Code").
 
-Shadow of Dust Source Code is free software: you can redistribute it and/or modify
+Doom 3 Source Code is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Shadow of Dust Source Code is distributed in the hope that it will be useful,
+Doom 3 Source Code is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Shadow of Dust Source Code.  If not, see <http://www.gnu.org/licenses/>.
+along with Doom 3 Source Code.  If not, see <http://www.gnu.org/licenses/>.
 
-In addition, the Shadow of Dust Source Code is also subject to certain additional terms. You should have received a copy of these additional terms immediately following the terms and conditions of the GNU General Public License which accompanied the Shadow of Dust Source Code.  If not, please request a copy in writing from id Software at the address below.
+In addition, the Doom 3 Source Code is also subject to certain additional terms. You should have received a copy of these additional terms immediately following the terms and conditions of the GNU General Public License which accompanied the Doom 3 Source Code.  If not, please request a copy in writing from id Software at the address below.
 
 If you have questions concerning this license or the applicable additional terms, you may contact in writing id Software LLC, c/o ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 
@@ -166,7 +166,7 @@ extern PFNGLDEPTHBOUNDSEXTPROC              qglDepthBoundsEXT;
 //===========================================================================
 
 // non-windows systems will just redefine qgl* to gl*
-#ifndef _WIN32
+#if 1
 
 #include "qgl_linked.h"
 
@@ -540,6 +540,6 @@ extern int  ( WINAPI * qwglGetLayerPaletteEntries)(HDC, int, int, int,
 extern BOOL ( WINAPI * qwglRealizeLayerPalette)(HDC, int, BOOL);
 extern BOOL ( WINAPI * qwglSwapLayerBuffers)(HDC, UINT);
 
-#endif	// _WIN32
+#endif
 
 #endif
