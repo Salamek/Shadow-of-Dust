291caa214cc3d61aad8364d10603adedd601014b
Adam Schubert <feartohell@seznam.cz>
2012-01-04 20:21:10 +0100

    Mess cleaning

--- neo/sys/win32/win_local.h
+++ neo/sys/win32/win_local.h
@@ -48,29 +48,11 @@ If you have questions concerning this license or the applicable additional terms
 #include "renderer/wglext.h"		// windows OpenGL extensions
 #include "sys/sys_public.h"
 
-// WGL_ARB_extensions_string
-extern	PFNWGLGETEXTENSIONSSTRINGARBPROC wglGetExtensionsStringARB;
-
 // WGL_EXT_swap_interval
 extern	PFNWGLSWAPINTERVALEXTPROC wglSwapIntervalEXT;
 
 // WGL_ARB_pixel_format
-extern	PFNWGLGETPIXELFORMATATTRIBIVARBPROC wglGetPixelFormatAttribivARB;
-extern	PFNWGLGETPIXELFORMATATTRIBFVARBPROC wglGetPixelFormatAttribfvARB;
 extern	PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB;
-
-// WGL_ARB_pbuffer
-extern	PFNWGLCREATEPBUFFERARBPROC	wglCreatePbufferARB;
-extern	PFNWGLGETPBUFFERDCARBPROC	wglGetPbufferDCARB;
-extern	PFNWGLRELEASEPBUFFERDCARBPROC	wglReleasePbufferDCARB;
-extern	PFNWGLDESTROYPBUFFERARBPROC	wglDestroyPbufferARB;
-extern	PFNWGLQUERYPBUFFERARBPROC	wglQueryPbufferARB;
-
-// WGL_ARB_render_texture
-extern	PFNWGLBINDTEXIMAGEARBPROC		wglBindTexImageARB;
-extern	PFNWGLRELEASETEXIMAGEARBPROC	wglReleaseTexImageARB;
-extern	PFNWGLSETPBUFFERATTRIBARBPROC	wglSetPbufferAttribARB;
-
 
 #define	MAX_OSPATH			256
 
