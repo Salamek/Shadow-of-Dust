4af5cd94d62397826deda5bd444d67d113e1f106
Adam Schubert <feartohell@seznam.cz>
2012-01-05 20:39:06 +0100

    Added cleaning script

---
+++ neo/clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#cmake mess
+rm -rf "./CMakeFiles"
+rm -rf "./CMakeCache.txt"
+rm -rf "./cmake_install.cmake"
+rm -rf "./Makefile"
+
+#now compiled content
+rm -rf "./gamex86_64.so"
+rm -rf "./libidlibx86_64.a"
+rm -rf "./shadow-of-dust.x86_64"
