dbf6f6f957a47a0101c3f81cf8f2615f9e4724e9
Adam Schubert <feartohell@seznam.cz>
2012-01-08 03:52:23 +0100

    Moved jpeg_memory to separated file, cos its used in Cinematic.cpp and Image_files.cpp

--- neo/clean.sh
+++ neo/clean.sh
@@ -8,5 +8,5 @@ rm -rf "./Makefile"
 
 #now compiled content
 rm -rf "./gamex86_64.so"
-rm -rf "./libidlibx86_64.a"
-rm -rf "./shadow-of-dust.x86_64"
+rm -rf "./libidlib.a"
+rm -rf "./shadow-of-dust"
