d98fb2cd837b1ae3df8d208afbe65db45a4d4767
Adam Schubert <feartohell@seznam.cz>
2012-01-09 22:12:02 +0100

    Edited clean script, half of it is make clean work

--- neo/clean.sh
+++ neo/clean.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#cmake mess
+#compiled content
+make clean
+
+#now cmake mess
 rm -rf "./CMakeFiles"
 rm -rf "./CMakeCache.txt"
 rm -rf "./cmake_install.cmake"
 rm -rf "./Makefile"
 
-#now compiled content
-rm -rf "./gamex86_64.so"
-rm -rf "./libidlib.a"
-rm -rf "./shadow-of-dust"
+
