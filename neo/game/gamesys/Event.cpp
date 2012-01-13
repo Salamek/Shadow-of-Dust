3bc31a079589148f756d61ec2f723d7023505adb
Adam Schubert <feartohell@seznam.cz>
2012-01-13 21:04:10 +0100

    Save/load fix, loads must faster on x86_64

--- neo/game/gamesys/Event.cpp
+++ neo/game/gamesys/Event.cpp
@@ -99,6 +99,8 @@ idEventDef::idEventDef( const char *command, const char *formatspec, char return
 			break;
 
 		case D_EVENT_INTEGER :
+		case D_EVENT_ENTITY :
+		case D_EVENT_ENTITY_NULL :
 			argsize += sizeof( intptr_t );
 			break;
 
@@ -108,14 +110,6 @@ idEventDef::idEventDef( const char *command, const char *formatspec, char return
 
 		case D_EVENT_STRING :
 			argsize += MAX_STRING_LEN;
-			break;
-
-		case D_EVENT_ENTITY :
-			argsize += sizeof( idEntityPtr<idEntity> );
-			break;
-
-		case D_EVENT_ENTITY_NULL :
-			argsize += sizeof( idEntityPtr<idEntity> );
 			break;
 
 		case D_EVENT_TRACE :
@@ -772,24 +766,27 @@ void idEvent::Save( idSaveGame *savefile ) {
 			switch( format[ i ] ) {
 				case D_EVENT_FLOAT :
 					savefile->WriteFloat( *reinterpret_cast<float *>( dataPtr ) );
-					size += sizeof( float );
+					size += sizeof( intptr_t );
 					break;
 				case D_EVENT_INTEGER :
+					savefile->WriteInt( *reinterpret_cast<int *>( dataPtr ) );
+					size += sizeof( intptr_t );
+					break;
 				case D_EVENT_ENTITY :
 				case D_EVENT_ENTITY_NULL :
-					savefile->WriteInt( *reinterpret_cast<int *>( dataPtr ) );
-					size += sizeof( int );
+					reinterpret_cast< idEntityPtr<idEntity> * >( dataPtr )->Save(savefile);
+					size += sizeof( intptr_t );
 					break;
 				case D_EVENT_VECTOR :
 					savefile->WriteVec3( *reinterpret_cast<idVec3 *>( dataPtr ) );
-					size += sizeof( idVec3 );
+					size += E_EVENT_SIZEOF_VEC;
 					break;
 				case D_EVENT_STRING :
 					s.Clear();
 					s.Append(reinterpret_cast<char *>(dataPtr));
 					savefile->WriteString(s);
 					size += MAX_STRING_LEN;
-				break;
+					break;
 				case D_EVENT_TRACE :
 					validTrace = *reinterpret_cast<bool *>( dataPtr );
 					savefile->WriteBool( validTrace );
@@ -844,6 +841,7 @@ void idEvent::Restore( idRestoreGame *savefile ) {
 	idEvent	*event;
 	const char	*format;
 	idStr s;
+
 	savefile->ReadInt( num );
 
 	for ( i = 0; i < num; i++ ) {
@@ -887,23 +885,26 @@ void idEvent::Restore( idRestoreGame *savefile ) {
 				switch( format[ j ] ) {
 					case D_EVENT_FLOAT :
 						savefile->ReadFloat( *reinterpret_cast<float *>( dataPtr ) );
-						size += sizeof( float );
+						size += sizeof( intptr_t );
 						break;
 					case D_EVENT_INTEGER :
+						savefile->ReadInt( *reinterpret_cast<int *>( dataPtr ) );
+						size += sizeof( intptr_t );
+						break;
 					case D_EVENT_ENTITY :
 					case D_EVENT_ENTITY_NULL :
-						savefile->ReadInt( *reinterpret_cast<int *>( dataPtr ) );
-						size += sizeof( int );
+						reinterpret_cast< idEntityPtr<idEntity> * >( dataPtr )->Restore(savefile);
+						size += sizeof( intptr_t );
 						break;
 					case D_EVENT_VECTOR :
 						savefile->ReadVec3( *reinterpret_cast<idVec3 *>( dataPtr ) );
-						size += sizeof( idVec3 );
+						size += E_EVENT_SIZEOF_VEC;
 						break;
 					case D_EVENT_STRING :
 						savefile->ReadString(s);
 						idStr::Copynz(reinterpret_cast<char *>(dataPtr), s, MAX_STRING_LEN);
 						size += MAX_STRING_LEN;
-					break;
+						break;
 					case D_EVENT_TRACE :
 						savefile->ReadBool( *reinterpret_cast<bool *>( dataPtr ) );
 						size += sizeof( bool );
